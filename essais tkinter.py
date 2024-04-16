8d9c1962908a8fd5846ed626e6b62e2ab2554238
Valentine <155440159+Valentine23012@users.noreply.github.com>
2024-04-16 14:50:24 +0200

    Add files via upload

---
+++ essais tkinter.py
@@ -0,0 +1,9 @@
+from tkinter import *
+fenetre = Tk()
+
+fenetre.title("Projet final")
+fenetre.geometry("598x420")
+fenetre.config(background="#FFF5E5")
+label_titre = Label(fenetre, text="Bienvenue sur le jeu")
+label_titre.pack()
+fenetre.mainloop()
