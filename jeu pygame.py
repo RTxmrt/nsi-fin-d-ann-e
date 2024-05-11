001e4578b8b3e6a8865a9ccc0cb6a66d63198b57
RTxmrt <150149652+RTxmrt@users.noreply.github.com>
2024-05-11 12:06:24 +0200

    Update jeu pygame.py

--- jeu pygame.py
+++ jeu pygame.py
@@ -1,14 +1,46 @@
 import pygame
+import math
+import random
 pygame.init()
 
 class Game:
     def __init__(self):
+        self.is_playing = False
         self.all_players = pygame.sprite.Group()
         self.player = player(self)
         self.all_players.add(self.player)
         self.all_monsters = pygame.sprite.Group()
         self.pressed = {}
+
+    def start(self):
+        self.is_playing = True
         self.spawn_monster()
+        self.spawn_monster()
+
+    def game_over(self):
+        self.all_monsters = pygame.sprite.Group()
+        self.player.health = self.player.max_health
+        self.is_playing = False
+    def update(self, surface):
+        surface.blit(self.player.image, self.player.rect)
+
+        self.player.update_health_bar(surface)
+
+        for projectile in self.player.all_projectiles:
+            projectile.move()
+
+        for monster in self.all_monsters:
+            monster.forward()
+            monster.update_health_bar(surface)
+
+        self.player.all_projectiles.draw(surface)
+
+        self.all_monsters.draw(surface)
+
+        if self.pressed.get(pygame.K_RIGHT) and self.player.rect.x + self.player.rect.width < surface.get_width():
+            self.player.move_right()
+        elif self.pressed.get(pygame.K_LEFT) and self.player.rect.x > 0:
+            self.player.move_left()
 
     def check_collision(self, sprite, group):
         return pygame.sprite.spritecollide(sprite, group, False, pygame.sprite.collide_mask)
@@ -19,8 +51,8 @@ class player(pygame.sprite.Sprite):
     def __init__(self, game):
         super().__init__()
         self.game = game
-        self.health = 100
-        self.max_health = 100
+        self.health = 200
+        self.max_health = 200
         self.attack = 10
         self.velocity = 2
         self.all_projectiles = pygame.sprite.Group()
@@ -29,6 +61,19 @@ class player(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = 400
         self.rect.y = 470
+
+    def damage(self, amount):
+        if self.health - amount > amount:
+            self.health -= amount
+        else:
+            self.game.game_over()
+    def update_health_bar(self, surface):
+        bar_color = (100, 164, 60)
+        back_bar_color = (60, 63, 60)
+        bar_position = [self.rect.x + 10, self.rect.y -20, self.health, 5]
+        back_bar_position = [self.rect.x + 10, self.rect.y -20, self.max_health, 5]
+        pygame.draw.rect(surface, back_bar_color, back_bar_position)
+        pygame.draw.rect(surface, bar_color, bar_position)
 
     def launch_projectile(self):
         self.all_projectiles.add(Projectile(self))
@@ -42,7 +87,7 @@ class player(pygame.sprite.Sprite):
 class Projectile(pygame.sprite.Sprite):
     def __init__(self, player):
         super().__init__()
-        self.velocity = 2
+        self.velocity = 5
         self.player = player
         self.image = pygame.image.load('glace3.png')
         self.image = pygame.transform.scale(self.image, (70, 70))
@@ -61,8 +106,9 @@ class Projectile(pygame.sprite.Sprite):
     def move(self):
         self.rect.x += self.velocity
         self.rotate()
-        if self.player.game.check_collision(self, self.player.game.all_monsters):
+        for monster in self.player.game.check_collision(self, self.player.game.all_monsters):
             self.remove()
+            monster.damage(self.player.attack)
         if self.rect.x > 1080:
             self.remove()
 
@@ -72,17 +118,34 @@ class Monster(pygame.sprite.Sprite):
         self.game = game
         self.health = 100
         self.max_health = 100
-        self.attack = 5
+        self.attack = 0.5
         self.image = pygame.image.load('traveler.png')
         self.rect = self.image.get_rect()
-        self.image = pygame.transform.scale(self.image, (200, 200))
-        self.rect.x = 1000
-        self.rect.y = 500
-        self.velocity = 1
+        self.image = pygame.transform.scale(self.image, (220, 220))
+        self.rect.x = 1000 + random.randint(0, 300)
+        self.rect.y = 483
+        self.velocity = random.randint(1, 2)
+
+    def damage(self, amount):
+        self.health -= amount
+        if self.health <= 0:
+            self.rect.x = 1000 + random.randint(0, 300)
+            self.velocity = random.randint(1, 3)
+            self.health = self.max_health
+
+    def update_health_bar(self, surface):
+        bar_color = (100, 164, 60)
+        back_bar_color = (60, 63, 60)
+        bar_position = [self.rect.x + 10, self.rect.y -20, self.health, 5]
+        back_bar_position = [self.rect.x + 10, self.rect.y -20, self.max_health, 5]
+        pygame.draw.rect(surface, back_bar_color, back_bar_position)
+        pygame.draw.rect(surface, bar_color, bar_position)
 
     def forward(self):
         if not self.game.check_collision(self, self.game.all_players):
             self.rect.x -= self.velocity
+        else:
+            self.game.player.damage(self.attack)
 
 
 pygame.init()
@@ -92,6 +155,18 @@ surface = pygame.display.set_mode((1080, 720))
 background = pygame.image.load("plage.jpg")
 background = pygame.transform.scale(background, (1080, 720))
 
+banner = pygame.image.load("plagebanniere.jpg")
+banner = pygame.transform.scale(banner, (900, 350))
+banner_rect = banner.get_rect()
+banner_rect.x = math.ceil(surface.get_width() / 10)
+banner_rect.y = math.ceil(surface.get_width() / 10)
+
+play_button = pygame.image.load("play.png")
+play_button = pygame.transform.scale(play_button, (450, 450))
+play_button_rect = play_button.get_rect()
+play_button_rect.x = math.ceil(surface.get_width() / 3.33)
+play_button_rect.y = math.ceil(surface.get_width() / 4)
+
 game = Game()
 
 
@@ -100,22 +175,14 @@ running = True
 while running:
 
     surface.blit(background, (0, 0))
-    surface.blit(game.player.image, game.player.rect)
-    for projectile in game.player.all_projectiles:
-        projectile.move()
-    for monster in game.all_monsters:
-        monster.forward()
-    game.player.all_projectiles.draw(surface)
 
-    game.all_monsters.draw(surface)
-
-
-    if game.pressed.get(pygame.K_RIGHT) and game.player.rect.x + game.player.rect.width < surface.get_width():
-        game.player.move_right()
-    elif game.pressed.get(pygame.K_LEFT) and game.player.rect.x > 0:
-        game.player.move_left()
     print(game.player.rect.x)
 
+    if game.is_playing:
+        game.update(surface)
+    else:
+        surface.blit(banner, banner_rect)
+        surface.blit(play_button, play_button_rect)
     pygame.display.flip()
 
     for event in pygame.event.get():
@@ -131,3 +198,7 @@ while running:
 
         elif event.type == pygame.KEYUP:
             game.pressed[event.key] = False
+
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            if play_button_rect.collidepoint(event.pos):
+                game.start()
